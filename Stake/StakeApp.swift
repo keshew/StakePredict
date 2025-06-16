90b3a4c6b31cb3c220f02f2449655bf1a6f4cc11
keshew <artyom.korotckov2017@yandex.ru>
2025-06-16 13:03:40 +0500

    Initial Commit

---
+++ Stake/StakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  StakeApp.swift
+//  Stake
+//
+//  Created by Артём Коротков on 16.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct StakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
